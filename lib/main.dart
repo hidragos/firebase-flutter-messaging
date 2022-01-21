6116fe152e7224953353a0e01bdafe118a325581
Dragos Iliescu <dragos@allroundzonwering.nl>
2022-01-22 00:36:35 +0200

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:firebase_flutter_messaging/main-view.dart';
 import 'package:firebase_flutter_messaging/message-input.dart';
 import 'package:firebase_flutter_messaging/messages-list.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const Main());
 }
 
